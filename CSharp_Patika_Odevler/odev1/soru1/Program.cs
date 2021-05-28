a49f5841fc77e981e961f58d368a359561af158d
Busra Altun <busraltunb@gmail.com>
2021-05-28 16:35:05 +0300

    Patika.dev üzerinde yer alan C# eğitimi içerisinde verilen Ödev1 de yer alan C# algoritma sorularının çözümleri yapılmıştır.

---
+++ CSharp_Patika_Odevler/odev1/soru1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Sayı Giriniz : ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > 0)
+                {
+                    for (int i = 0; i < number; i++)
+                    {
+                        
+                        if ( i %2 == 0)
+                        {
+                            Console.Write( i + "-" );
+                        }
+
+                        
+                    }
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(" !!! Lütfen Sıfırdan Büyük Bir Sayı Giriniz !!! ");
+                }
+            }
+        }
+    }
+}
---
+++ CSharp_Patika_Odevler/odev1/soru2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace soru2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("İlk pozitif sayıyı giriniz :");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("İkinci sayıyı giriniz :");
+            int number2 = int.Parse(Console.ReadLine());
+            if (number1 > 0 && number2 > 0)
+            {
+                Console.WriteLine("Lütfen" + " " + number1 + " adet sayı girişi yapınız :");
+
+
+                for (int i = 0; i < number1; i++)
+                {
+                    Console.Write("{0}.sayı : ", i + 1);
+                    int value = int.Parse(Console.ReadLine());
+
+                    if (value == number2 || value % 2 == 0)
+                    {
+
+
+                        Console.WriteLine(value + " " + number2 + " eşit veya tam bölünen bir sayıdır.");
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine(number2 + " " + value + " ile aynı veya tam bölünen sayı değildir.");
+                    }
+
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Lütfen sayıları pozitif giriniz !!! ");
+            }
+
+        }
+    }
+}
