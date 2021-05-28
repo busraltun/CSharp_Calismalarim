39aa23ce102333aacbc34bd9f0827350987564af
Busra Altun <busraltunb@gmail.com>
2021-05-28 21:32:41 +0300

    Patika C# eğitiminde verilen Ödev1 için 3.sorunun çözümü eklendi

--- CSharp_Patika_Odevler/odev1/soru3/Program.cs
+++ CSharp_Patika_Odevler/odev1/soru3/Program.cs
@@ -4,9 +4,35 @@ namespace soru3
 {
     class Program
     {
+        /*
+        Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+        Sonrasında kullanıcıdan n adet kelime girmesi isteyin.
+         Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Lütfen pozitif bir sayı giriniz:");
+            int number = int.Parse(Console.ReadLine());
+            if (true)
+            {
+                Console.WriteLine("Lütfen" + " " + number + " " + "tane kelime giriniz :");
+                string[] word = new string[number];
+                for (int i = 0; i < number; i++)
+                {
+                    Console.WriteLine(i + 1 + ".kelime:");
+                    word[i] = Convert.ToString(Console.ReadLine());
+                    int rank = word.Length - 1;
+
+                    for (int j = rank; j >= 0; --j)
+                    {
+                        Console.WriteLine(word[j]);
+
+                    }
+
+                }
+
+            }
+
         }
     }
 }
