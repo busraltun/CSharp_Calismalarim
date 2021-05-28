95be2724d67c8f23689c62ac3234e980494050a1
Busra Altun <busraltunb@gmail.com>
2021-05-28 22:06:24 +0300

    Patika üzerinde yer alan C# eğitiminin ödev 1 kısmının 4.sorusu çözüldü

--- CSharp_Patika_Odevler/odev1/soru4/Program.cs
+++ CSharp_Patika_Odevler/odev1/soru4/Program.cs
@@ -4,9 +4,37 @@ namespace soru4
 {
     class Program
     {
+        /*
+        Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin.
+         Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var letterNum = 0;
+            var wordNum = 0;
+            bool newWord = true;
+
+            Console.WriteLine("Lütfen bir cümle yazın : ");
+            string sentence = Console.ReadLine();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (char.IsLetter(sentence[i]))
+                {
+                    letterNum++;
+                    if (newWord)
+                    {
+                        wordNum++;
+                        newWord = false;
+                    }
+                }
+                else if (char.IsSeparator(sentence[i]))
+                {
+                    newWord = true;
+                }
+            }
+
+ Console.WriteLine($"Toplam {letterNum} harf, {wordNum} kelime");
         }
+
     }
 }
