63b4014ebe5ef791df215389f847ad8e0cfac2ee
Busra Altun <busraltunb@gmail.com>
2021-05-28 16:39:55 +0300

    Kullanıcının konsoldan girmiş olduğu sayılardan çift olanları konsola yazdırma işlemi yapıldı

--- CSharp_Patika_Odevler/odev1/soru1/Program.cs
+++ CSharp_Patika_Odevler/odev1/soru1/Program.cs
@@ -4,6 +4,9 @@ namespace odev1
 {
     class Program
     {
+/*Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
+Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın. */
         static void Main(string[] args)
         {
             while (true)
