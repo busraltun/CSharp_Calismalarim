ccb59a7a374d149a62804c945a3c5cd54687f798
Busra Altun <busraltunb@gmail.com>
2021-06-02 23:33:55 +0300

    Telefon Rehberi Uygulaması

---
+++ Proje1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
